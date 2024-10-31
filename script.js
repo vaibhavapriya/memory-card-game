695c339bba0812274e7b755c3a16fda025c53eff
vaibhavapriya <vaibhavapriyandv@gmail.com>
2024-10-31 14:33:29 +0530

    debugged

--- script.js
+++ script.js
@@ -4,9 +4,6 @@ let shuffledA= [];//toggledCardsArray
 const restart = document.getElementById('restart');
 const cards = document.getElementsByClassName('card');
 let movesDynamic = document.getElementById('move'); // movesDisplay
-
-//let movesDynamic = document.querySelector('move'); //movesDisplay
-// let all images not wrote
 let allImages = document.getElementsByClassName('card-front');
 
 const imagesLinkArray=[
@@ -14,36 +11,48 @@ const imagesLinkArray=[
     { id:2,image:"images/c2.jpg",alt:10},
     { id:3,image:"images/c3.jpg",alt:7},
     { id:4,image:"images/c4.jpg",alt:6},
-    { id:1,image:"images/c1.jpg",alt:5},
-    { id:2,image:"images/c2.jpg",alt:10},
-    { id:3,image:"images/c3.jpg",alt:7},
-    { id:4,image:"images/c4.jpg",alt:6}
-    //{id:}
+    { id:5,image:"images/c1.jpg",alt:5},
+    { id:6,image:"images/c2.jpg",alt:10},
+    { id:7,image:"images/c3.jpg",alt:7},
+    { id:8,image:"images/c4.jpg",alt:6}
+    //{id:,image:,alt}
 ];
+//debug clicking same image
 
 //checking for the last clicked and current 
 //clicked cards and applying changes accordingly
 for (var i = 0; i < cards.length; i++) {
     cards[i].addEventListener('click', function () {
+        //avoid retogging
+        if (this.classList.contains("toggled") || shuffledA.length === 2) return;
         this.classList.add("toggled");
+        //adding the 2 cards to check
         shuffledA.push(this);
         let thisImgSrc = this.querySelector('.card-front').src;
         let previousImgSrc = shuffledA[shuffledA.length - 2].querySelector('.card-front').src;
+        // let thisImgId = this.querySelector('.card-front').id;
+        // let previousImgId = shuffledA[shuffledA.length - 2].querySelector('.card-front').id;
+        // if (thisImgId === previousImgId) {
+        //     shuffledA.forEach(function (el) {
+        //         setTimeout(() => {
+        //             el.classList.remove("toggled");
+        //         }, 100);
+        //     })
+        //     move++;           
+        // }
         if(thisImgSrc !== previousImgSrc) {
             shuffledA.forEach(function (el) {
                 setTimeout(() => {
                     el.classList.remove("toggled");
-                }, 500);
+                }, 1500);
             })
             shuffledA.length = 0;
             move++;
-            // alert(` ${move} moves.`)
         }
         else{
             shuffledA.length = 0;
             move++;
             winCount++;
-            // alert(` ${move} moves.`)
             // alert(` ${winCount} count.`)
         }
         movesDynamic.innerText = `Moves: ${move}`;
